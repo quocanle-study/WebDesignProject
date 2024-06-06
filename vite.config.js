094acb059a31ba0579e4e1f22ccc0231a3cb3412
Quoc An Le <lequocan2004@gmail.com>
2024-06-06 15:58:26 +0700

    Add Vite configuration for multiple entry points

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        shop: 'shop.html',
+        product: 'product.html',
+      },
+    },
+  },
+});
