6e20e0dea9b832fec42f14c5e3f1612241f45f8f
Quoc An Le <lequocan2004@gmail.com>
2024-06-20 21:39:25 +0700

    add database, shop, product page have not fi yetx

--- webapp/src/controllers/product-controller.js
+++ webapp/src/controllers/product-controller.js
@@ -1,11 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const getProducts = async (req, res) => {
     // TODO: Fetch products from database
-    return res.render('pages/shop');
+    try {
+        const product = await prisma.product.findUnique({
+            where: { id: Number(req.params.id) },
+            include: {
+                productCategories: {
+                    include: {
+                        category: true,
+                    },
+                },
+                images: true,
+            },
+        });
+        return res.render('pages/shop', { products });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('An error occurred while fetching products');
+    }
 };
 
 const getProductById = async (req, res) => {
     // TODO: Fetch product by id from database
-    return res.render('pages/product');
+    try {
+        const product = await prisma.product.findUnique({
+            where: { id: Number(req.params.id) },
+            include: {
+                productCategories: {
+                    include: {
+                        category: true,
+                    },
+                },
+                images: true,
+            },
+        });
+
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+
+        return res.render('pages/product', { product });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('An error occurred while fetching the product');
+    }
 };
 
 export { getProducts, getProductById };
