6b5faafb998f29ce899dfce70bffd656220d1f02
Quoc An Le <lequocan2004@gmail.com>
2024-06-24 15:53:39 +0700

    Update vite.config.js to include base path for deployment

--- mockup/vite.config.js
+++ mockup/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   build: {
     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg', '**/*.webp'],
     rollupOptions: {
